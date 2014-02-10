d7aaf09a8360af812406e36b34d0735831612c02
Toby Dylan Hocking <tdhock5@gmail.com>
2014-02-10 17:35:50 -0500

    genotypes data

--- tests/genotype.R
+++ tests/genotype.R
@@ -18,3 +18,23 @@ stopifnot(ncol(match) == ncol(expect))
 stopifnot(!is.null(colnames(match)))
 stopifnot(c("A", "B") == colnames(match))
 stopifnot(expect == match)
+
+data(genotypes)
+ref.alt <- sapply(genotypes, function(x)nchar(as.character(x)))
+##table(ref.alt[,1:2])
+stopifnot(ref.alt < 1e3) #bigger will crash regexp engine.
+group.pat <- sprintf("%s|%s", genotypes$Ref, gsub(",", "|", genotypes$Alt))
+genotypes[grepl("N", group.pat),]
+pat <- sprintf("^(?<A>%s)(?<B>%s)$", group.pat, group.pat)
+ab <- str_match_each(genotypes$genotype, pat)
+
+## Check that all NN/NAs are detected correctly.
+is.nn <- is.na(ab[,1])
+nn <- genotypes[is.nn,]
+stopifnot(nn$genotype == "NN")
+
+## Check that all others are detected correctly.
+not.nn <- data.frame(genotypes, ab)[!is.nn,]
+with(not.nn, {
+  stopifnot(identical(paste0(A, B), genotype))
+})
